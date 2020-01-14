f8e98e1cd352a67d0dc03dd47c7131e30d1c8ce7
Fabrício Henrique <fabricioh.silva@live.com>
2020-01-13 23:25:34 -0300

    WIP: aboutMe component

--- src/app/sections/about-me/about-me.component.ts
+++ src/app/sections/about-me/about-me.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import resume from '../../../resume.json';
+
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
+  Resume: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.Resume = resume;
   }
 
 }
